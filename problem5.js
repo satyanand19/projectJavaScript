96d0420c029a2a8c8018a09d50a26e6c6427769a
Satyanand Yadav <satyanand.yadav.19.1@mountblue.tech>
2022-03-10 17:51:01 +0530

    new updated file

--- problem5.js
+++ problem5.js
@@ -2,23 +2,16 @@
 
 
 function olderCar(inventory) {
-    let carYearDetails=[];
-    for(let i=0;i<inventory.length;i++)
-    {
-        if(inventory[i]['car_year']<2000) {
+    let carYearDetails = [];
+    for (let i = 0; i < inventory.length; i++) {
+        if (inventory[i]['car_year'] < 2000) {
             carYearDetails.push(inventory[i]);
         }
-        
+
     }
-    for (let i=0;i<carYearDetails.length;i++) {
+    for (let i = 0; i < carYearDetails.length; i++) {
         console.log(carYearDetails[i]);
     }
-    // let countCar=0;
-    // for(let i=0;i<carYearDetails.length;i++) {
-    //     if(carYearDetails[i]<2000) {
-    //         countCar++;
-    //     }
-    // }
-    console.log('count of all the cars Older than 2000 year : '+carYearDetails.length);
+    console.log('count of all the cars Older than 2000 year : ' + carYearDetails.length);
 }
-module.exports = olderCar ;
+module.exports = olderCar;
